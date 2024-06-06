c5e48027a5b12e27d737341785298c8d2b843ddd
Keekay-OD <omardiaz2012@gmail.com>
2024-06-06 09:38:22 -0700

    Added version check to startup for consumer

--- server.lua
+++ server.lua
@@ -1,4 +1,79 @@
 local QBCore = exports['qb-core']:GetCoreObject()
+local repositoryOwner = "Keekay-OD"
+local repositoryName = "vibes-ems"
+
+local function GetLatestReleaseVersion()
+    local localVersion = GetResourceMetadata(GetCurrentResourceName(), "version", 0)
+    print("^2Vibes^7-^2EMS v^3" .. localVersion .. " ^7- ^2EMS Script by ^1Keekay_YKTV^7")
+    
+    print("^2Checking for updates...")
+    
+    local loadingFrames = {"^7[^1-^7]", "^7[^1\\^7]", "^7[^1|^7]", "^7[^1/^7]"}
+    local loadingIndex = 1
+    
+    local function printLoading()
+        Citizen.CreateThread(function()
+            while loadingIndex > 0 do
+                Citizen.Wait(200)
+                if loadingIndex > #loadingFrames then
+                    loadingIndex = 1
+                end
+                if loadingIndex > 0 then
+                    print(loadingFrames[loadingIndex] .. " ^2Retrieving latest release information...")
+                    loadingIndex = loadingIndex + 1
+                end
+            end
+        end)
+    end
+    
+    printLoading()
+    
+    local request = PerformHttpRequest("https://api.github.com/repos/" .. repositoryOwner .. "/" .. repositoryName .. "/releases/latest", function(statusCode, responseText, headers)
+        if statusCode == 200 then
+            local releaseData = json.decode(responseText)
+            local latestVersion = releaseData.tag_name
+            if string.sub(latestVersion, 1, 1) == "v" then
+                latestVersion = string.sub(latestVersion, 2)
+            end
+            
+            -- Stop the loading animation
+            loadingIndex = 0
+            
+            -- Version comparison logic
+            if localVersion < latestVersion then
+				Wait(1000)
+                print("^1Your version is out of date!")
+                print("^7You are using version: ^1" .. localVersion)
+                print("^7A new version is available: ^2" .. latestVersion)
+                -- Perform any additional actions you want to take when a new version is available
+            elseif localVersion > latestVersion then
+				Wait(1000)
+                print("^9Shoutouts to you for being a part of the Vibes Dev Team.")
+				Wait(1000)
+                print("^7You are using a newer version: ^9" .. tostring(localVersion))
+				Wait(1000)
+                print("^7The latest public release version is: ^2" .. latestVersion)
+                -- Perform any additional actions for testers using a newer version
+            else
+				Wait(1000)
+                print("^7You are using the latest version: ^2" .. localVersion)
+            end
+
+        else
+            -- Stop the loading animation
+            loadingIndex = 0
+            
+            print("^1Failed to retrieve the latest release information. Status Code: " .. statusCode)
+        end
+    end, "GET", "", { ["Content-Type"] = "application/json" })
+end
+
+-- Register the resource start event handler
+AddEventHandler('onResourceStart', function(resourceName)
+    if GetCurrentResourceName() == resourceName then
+        GetLatestReleaseVersion()
+    end
+end)
 
 QBCore.Functions.CreateCallback('hhfw:docOnline', function(source, cb)
 	local src = source
