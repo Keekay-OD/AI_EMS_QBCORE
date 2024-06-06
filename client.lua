0e87541950b7ecfe4f06e91d16ab832d0a1cdc5d
Keekay-OD <omardiaz2012@gmail.com>
2024-06-06 10:17:13 -0700

    Support for mythic and okok notify added

--- client.lua
+++ client.lua
@@ -141,26 +141,37 @@ function DoctorNPC()
 	end
 
 	TaskPlayAnim(test1, "mini@cpr@char_a@cpr_str","cpr_pumpchest",1.0, 1.0, -1, 9, 1.0, 0, 0, 0)
-	QBCore.Functions.Progressbar("revive_doc", "The doctor is giving you medical aid", Config.ReviveTime, false, false, {
-		disableMovement = false,
-		disableCarMovement = false,
-		disableMouse = false,
-		disableCombat = true,
-	}, {}, {}, {}, function() -- Done
-		ClearPedTasks(test1)
-		Citizen.Wait(500)
-        	TriggerEvent("hospital:client:Revive")
-		StopScreenEffect('DeathFailOut')	
-		Notify("Your treatment is done, you were charged: "..Config.Price, "success")
-		RemovePedElegantly(test1)
-		DeleteEntity(test)
-		Wait(5000)
-		DeleteEntity(test1)
-		spam = true
-	end)
+    QBCore.Functions.Progressbar("revive_doc", "The doctor is giving you medical aid", Config.ReviveTime, false, false, {
+        disableMovement = false,
+        disableCarMovement = false,
+        disableMouse = false,
+        disableCombat = true,
+    }, {}, {}, {}, function() -- Done
+        ClearPedTasks(test1)
+        Citizen.Wait(500)
+        TriggerEvent("hospital:client:Revive")
+        StopScreenEffect('DeathFailOut')
+        Notify("Your treatment is done, you were charged: " .. Config.Price, "success")
+        RemovePedElegantly(test1)
+        DeleteEntity(test)
+        Wait(5000)
+        DeleteEntity(test1)
+        spam = true
+    end)
 end
 
 
 function Notify(msg, state)
-    QBCore.Functions.Notify(msg, state)
+    if Config.NotifyType == "qb" then
+        QBCore.Functions.Notify(msg, state)
+    elseif Config.NotifyType == "okok" then
+        exports['okokNotify']:Alert("Doctor", msg, 5000, state, true)
+    elseif Config.NotifyType == "mythic" then
+        exports['mythic_notify']:DoHudText(state, msg)
+    elseif Config.NotifyType == "custom" and Config.CustomNotifyFunction then
+        Config.CustomNotifyFunction(msg, state)
+    else
+        -- Fallback to default notification or print to console
+        print("[Doctor NPC] " .. msg)
+    end
 end
--- config.lua
+++ config.lua
@@ -7,3 +7,5 @@ Config.Doctor = 1 -- Minimum Amount of EMS to work
 Config.Price = 150 -- Price to call EMS
 Config.ReviveTime = 10000  --in msec
 Config.UseHelpCommand = true -- /help command to show the help message
+Config.NotifyType = "okok" -- Options: "qb", "okok", "mythic", "custom"
+Config.CustomNotifyFunction = nil -- Set this to your custom notification function if NotifyType is "custom"
